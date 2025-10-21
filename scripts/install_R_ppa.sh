15208e052e47d9ff3006a32a64ef0c21d2a2bcef
orehren <o.rehren@gmail.com>
2025-10-21 09:43:00 +0200

    updated environment install scripts

--- scripts/install_R_ppa.sh
+++ scripts/install_R_ppa.sh
@@ -12,9 +12,9 @@ R_HOME=${R_HOME:-/usr/lib/R}
 
 #R_VERSION=${R_VERSION}
 
-apt-get update
+sudo apt-get update
 
-apt-get -y install --no-install-recommends \
+sudo apt-get -y install --no-install-recommends \
       ca-certificates \
       less \
       libopenblas-base \
@@ -25,31 +25,28 @@ apt-get -y install --no-install-recommends \
       gpg \
       gpg-agent
 
-echo "deb http://cloud.r-project.org/bin/linux/ubuntu ${UBUNTU_VERSION}-${CRAN_LINUX_VERSION}/" >> /etc/apt/sources.list
-
-gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | apt-key add -
+echo "Add the CRAN repository for Ubuntu Noble"
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu noble-cran40/"
 
 
 # Wildcard * at end of version will grab (latest) patch of requested version
-apt-get update && apt-get -y install --no-install-recommends r-base-dev=${R_VERSION}*
-
-rm -rf /var/lib/apt/lists/*
+sudo apt-get update && sudo apt-get -y install --no-install-recommends r-base-dev=${R_VERSION}*
 
 ## Add PPAs: NOTE this will mean that installing binary R packages won't be version stable.
 ##
 ## These are required at least for bionic-based images since 3.4 r binaries are
 
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen en_US.utf8
-/usr/sbin/update-locale LANG=${LANG}
+echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
+sudo locale-gen en_US.utf8
+sudo /usr/sbin/update-locale LANG=${LANG}
 
 Rscript -e "install.packages(c('littler', 'docopt'))"
 
 ## By default R_LIBS_SITE is unset, and defaults to this, so this is where `littler` will be.
 ## We set it here for symlinks, but don't make the env var persist (since it's already the default)
 R_LIBS_SITE=/usr/local/lib/R/site-library
-ln -s ${R_LIBS_SITE}/littler/examples/install.r /usr/local/bin/install.r
-ln -s ${R_LIBS_SITE}/littler/examples/install2.r /usr/local/bin/install2.r
-ln -s ${R_LIBS_SITE}/littler/examples/installGithub.r /usr/local/bin/installGithub.r
-ln -s ${R_LIBS_SITE}/littler/bin/r /usr/local/bin/r
+sudo ln -s ${R_LIBS_SITE}/littler/examples/install.r /usr/local/bin/install.r
+sudo ln -s ${R_LIBS_SITE}/littler/examples/install2.r /usr/local/bin/install2.r
+sudo ln -s ${R_LIBS_SITE}/littler/examples/installGithub.r /usr/local/bin/installGithub.r
+sudo ln -s ${R_LIBS_SITE}/littler/bin/r /usr/local/bin/r
---
+++ scripts/install_R_source.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+## Install R from source.
+##
+## In order of preference, first argument of the script, the R_VERSION variable.
+## ex. latest, devel, patched, 4.0.0
+##
+## 'devel' means the prerelease development version (Latest daily snapshot of development version).
+## 'patched' means the prerelease patched version (Latest daily snapshot of patched version).
+
+set -e
+
+R_VERSION=${1:-${R_VERSION:-"latest"}}
+PURGE_BUILDDEPS=${PURGE_BUILDDEPS:-"true"}
+
+# shellcheck source=/dev/null
+source /etc/os-release
+
+sudo apt-get update
+sudo apt-get -y install locales
+
+## Configure default locale
+LANG=${LANG:-"en_US.UTF-8"}
+sudo /usr/sbin/locale-gen --lang "${LANG}"
+sudo /usr/sbin/update-locale --reset LANG="${LANG}"
+
+export DEBIAN_FRONTEND=noninteractive
+
+R_HOME=${R_HOME:-"/usr/local/lib/R"}
+
+READLINE_VERSION=8
+if [ "${UBUNTU_CODENAME}" == "bionic" ]; then
+    READLINE_VERSION=7
+fi
+
+sudo apt-get install -y --no-install-recommends \
+    bash-completion \
+    ca-certificates \
+    file \
+    fonts-texgyre \
+    g++ \
+    gfortran \
+    gsfonts \
+    libblas-dev \
+    libbz2-* \
+    libcurl4 \
+    "libicu[0-9][0-9]" \
+    liblapack-dev \
+    libpcre2* \
+    libjpeg-turbo* \
+    libpangocairo-* \
+    libpng16* \
+    "libreadline${READLINE_VERSION}" \
+    libtiff* \
+    liblzma* \
+    libxt6 \
+    make \
+    tzdata \
+    unzip \
+    zip \
+    zlib1g
+
+BUILDDEPS="curl \
+    default-jdk \
+    devscripts \
+    libbz2-dev \
+    libcairo2-dev \
+    libcurl4-openssl-dev \
+    libpango1.0-dev \
+    libjpeg-dev \
+    libicu-dev \
+    libpcre2-dev \
+    libpng-dev \
+    libreadline-dev \
+    libtiff5-dev \
+    liblzma-dev \
+    libx11-dev \
+    libxt-dev \
+    perl \
+    rsync \
+    subversion \
+    tcl-dev \
+    tk-dev \
+    texinfo \
+    texlive-extra-utils \
+    texlive-fonts-recommended \
+    texlive-fonts-extra \
+    texlive-latex-recommended \
+    texlive-latex-extra \
+    x11proto-core-dev \
+    xauth \
+    xfonts-base \
+    xvfb \
+    wget \
+    zlib1g-dev"
+
+# shellcheck disable=SC2086
+sudo apt-get install -y --no-install-recommends ${BUILDDEPS}
+
+## Download R from 0-Cloud CRAN mirror or CRAN
+function download_r_src() {
+    wget "https://cloud.r-project.org/src/$1" -O "R.tar.gz" ||
+        wget "https://cran.r-project.org/src/$1" -O "R.tar.gz"
+}
+
+if [ "$R_VERSION" == "devel" ]; then
+    download_r_src "base-prerelease/R-devel.tar.gz"
+elif [ "$R_VERSION" == "patched" ]; then
+    download_r_src "base-prerelease/R-latest.tar.gz"
+elif [ "$R_VERSION" == "latest" ]; then
+    download_r_src "base/R-latest.tar.gz"
+else
+    download_r_src "base/R-${R_VERSION%%.*}/R-${R_VERSION}.tar.gz"
+fi
+
+tar xzf "R.tar.gz"
+cd R-*/
+
+R_PAPERSIZE=letter \
+    R_BATCHSAVE="--no-save --no-restore" \
+    R_BROWSER=xdg-open \
+    PAGER=/usr/bin/pager \
+    PERL=/usr/bin/perl \
+    R_UNZIPCMD=/usr/bin/unzip \
+    R_ZIPCMD=/usr/bin/zip \
+    R_PRINTCMD=/usr/bin/lpr \
+    LIBnn=lib \
+    AWK=/usr/bin/awk \
+    CFLAGS="-g -O2 -fstack-protector-strong -Wformat -Werror=format-security -Wdate-time -D_FORTIFY_SOURCE=2 -g" \
+    CXXFLAGS="-g -O2 -fstack-protector-strong -Wformat -Werror=format-security -Wdate-time -D_FORTIFY_SOURCE=2 -g" \
+    ./configure --enable-R-shlib \
+    --enable-memory-profiling \
+    --with-readline \
+    --with-blas \
+    --with-lapack \
+    --with-tcltk \
+    --with-recommended-packages
+
+make
+sudo make install
+sudo make clean
+
+## Add a library directory (for user-installed packages)
+sudo mkdir -p "${R_HOME}/site-library"
+sudo chown root:staff "${R_HOME}/site-library"
+sudo chmod g+ws "${R_HOME}/site-library"
+
+## Fix library path
+echo "R_LIBS=\${R_LIBS-'${R_HOME}/site-library:${R_HOME}/library'}" | sudo tee -a ${R_HOME}/etc/Renviron.site
+
+## Clean up from R source install
+cd ..
+sudo rm -rf /tmp/*
+sudo rm -rf R-*/
+sudo rm -rf "R.tar.gz"
+
+## Copy the checkbashisms script to local before remove devscripts package.
+## https://github.com/rocker-org/rocker-versioned2/issues/510
+sudo cp /usr/bin/checkbashisms /usr/local/bin/checkbashisms
+
+# shellcheck disable=SC2086
+if [ "${PURGE_BUILDDEPS}" != "false" ]; then
+    sudo apt-get remove --purge -y ${BUILDDEPS}
+fi
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
+sudo rm -rf /var/lib/apt/lists/*
+
+# Check the R info
+echo -e "Check the R info...\n"
+
+R -q -e "sessionInfo()"
+
+echo -e "\nInstall R from source, done!"
